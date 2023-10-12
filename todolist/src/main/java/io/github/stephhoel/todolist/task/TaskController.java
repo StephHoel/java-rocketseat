37e52a01afcc14397764f568d60138f9c7688c9b
Steph Hoel <steph.hoel@gmail.com>
2023-10-12 12:56:07 -0300

    feat: add list tasks

--- todolist/src/main/java/io/github/stephhoel/todolist/task/TaskController.java
+++ todolist/src/main/java/io/github/stephhoel/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package io.github.stephhoel.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,10 +32,16 @@ public class TaskController {
     } else if (currentDate.isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de término deve ser maior do que a data atual");
     } else if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor do que a data de término");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("A data de início deve ser menor do que a data de término");
     }
 
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    return this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
   }
 }
