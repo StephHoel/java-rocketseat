cbd1b61dd40df5875a101d793238a89aab489404
Steph Hoel <steph.hoel@gmail.com>
2023-10-12 12:17:41 -0300

    feat: set idUser based auth

--- todolist/src/main/java/io/github/stephhoel/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/io/github/stephhoel/todolist/filter/FilterTaskAuth.java
@@ -39,6 +39,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         var passwordVerify = BCrypt.verifyer().verify(credentials[1].toCharArray(), user.getPassword());
 
         if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
           filterChain.doFilter(request, response);
         } else {
           response.sendError(401);
--- todolist/src/main/java/io/github/stephhoel/todolist/task/TaskController.java
+++ todolist/src/main/java/io/github/stephhoel/todolist/task/TaskController.java
@@ -1,10 +1,14 @@
 package io.github.stephhoel.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,7 +18,8 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel) {
+  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    taskModel.setIdUser((UUID) request.getAttribute("idUser"));
     return this.taskRepository.save(taskModel);
   }
 }
